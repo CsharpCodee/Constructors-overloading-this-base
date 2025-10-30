401df28867821d944be4dd86278f96dd0bca7712
CsharpCodee <y55792381@gmail.com>
2025-10-30 03:01:42 +0300

    Add files via upload

---
+++ Program 4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic10._4
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point(double value)
+        {
+            this.x = value;
+            this.y = value;
+        }
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Точка: ({x}, {y})");
+        }
+        static void Main(string[] args)
+        {
+            Point a = new Point(3.5, 5.2);
+            a.Show();
+
+            Point b = new Point(6.1);
+            b.Show();
+
+            Point c = new Point();
+            c.Show();
+
+        }
+    }
+}
---
+++ Program 5.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic10._5
+{
+    class Animal
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"{name} издаёт звук.");
+        }
+    }
+
+    class Cat:Animal
+    {
+        private int lives;
+
+        public int Lives
+        {
+            get { return lives; }
+            set { lives = (value >= 1 && value <= 9) ? value : 0; }
+        }
+
+        public Cat(int lives, string name) : base(name)
+        {
+            this.lives = lives;
+            this.Name = name;
+        }
+
+        public Cat(string name) : base(name)
+        {
+            this.Name = name;
+            this.lives = 9;
+        }
+
+        public void Meow()
+        {
+            if (lives >= 1)
+            {
+                lives -= 1;
+                Console.WriteLine($"{Name} мяукнул, осталось жизней: {lives}.");
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточнно жизней.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat(6, "Dril");
+            cat1.Speak();
+            cat1.Meow();
+            cat1.Meow();
+
+            Cat cat2 = new Cat("Блиня");
+            cat2.Speak();
+            cat2.Meow();
+
+
+        }
+    }
+}
