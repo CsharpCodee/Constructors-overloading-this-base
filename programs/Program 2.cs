199f42fdaa6ebd691c56f6784a0b3f3fa217419b
CsharpCodee <y55792381@gmail.com>
2025-10-30 03:05:26 +0300

    Add files via upload

---
+++ programs/Program 2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic10._2
+{
+    //2
+    class Employee
+    {
+        private string name;
+        private double salary;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0 && value <= 1000000)
+                { salary = value; }
+                else if (value < 0)
+                { Console.WriteLine("Зарплата не может быть отрицательной!"); }
+            }
+        }
+
+        public Employee(string name, double salary)
+        {
+            this.name = name;
+            Salary = salary;
+        }
+        public Employee(string name)
+        {
+            this.name = name;
+            this.salary = 50000;
+        }
+
+        public void Work()
+        {
+            Console.WriteLine($"{name} работает, зарплата: {salary}.");
+        }
+
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Sad", -1000);
+            employee1.Work();
+
+            Employee employee2 = new Employee("Sue");
+            employee2.Work();
+        }
+
+    }
+}
